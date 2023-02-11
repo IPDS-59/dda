b016bdf8015b2bb1ad781e75bde664e148bb96f6
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2023-02-11 19:56:54 +0800

    🦄 refactor(js): update app.js
    initialize inertia app

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,22 @@
-import './bootstrap';
+import "./bootstrap";
+
+import { createApp, h } from "vue";
+import { createInertiaApp } from "@inertiajs/vue3";
+import route from "ziggy-js";
+
+// Plugin Initializations
+import Notifications from "notiwind";
+
+createInertiaApp({
+    resolve: (name) => {
+        const pages = import.meta.glob("./Pages/**/*.vue", { eager: true });
+        return pages[`./Pages/${name}.vue`];
+    },
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .mixin({ methods: { route } })
+            .use(plugin)
+            .use(Notifications)
+            .mount(el);
+    },
+});
