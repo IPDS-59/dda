1d30d620b9005fd7459eef8248c03252e0e52500
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2023-02-12 05:07:38 +0800

    🦄 refactor(js): initialize plugin
    initialize fontawesome plugin

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,18 @@ import route from "ziggy-js";
 // Plugin Initializations
 import Notifications from "notiwind";
 
+// Font Awesome Setup
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+library.add(fas);
+import { fab } from "@fortawesome/free-brands-svg-icons";
+library.add(fab);
+import { far } from "@fortawesome/free-regular-svg-icons";
+library.add(far);
+import { dom } from "@fortawesome/fontawesome-svg-core";
+dom.watch();
+
 createInertiaApp({
     resolve: (name) => {
         const pages = import.meta.glob("./Pages/**/*.vue", { eager: true });
@@ -19,6 +31,7 @@ createInertiaApp({
             .use(plugin)
             .use(Notifications)
             .use(VueLazyLoad)
+            .component("font-awesome-icon", FontAwesomeIcon)
             .mount(el);
     },
 });
