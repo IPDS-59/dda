e129d165bccbdc53b5fb3b2c2e3bfa001ee777a7
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2023-02-12 05:54:25 +0800

    ✨ feat(pages): add pages
    create landing pages using vue

--- resources/js/Pages/landing.vue
+++ resources/js/Pages/landing.vue
@@ -1,6 +1,19 @@
 
+<script lang="ts">
+import { Head, Link } from "@inertiajs/vue3";
+import { Popover, PopoverButton, PopoverPanel } from '@headlessui/vue';
+import { InformationCircleIcon, ViewColumnsIcon, NewspaperIcon, PlayIcon } from '@heroicons/vue/24/solid';
+</script>
+<script setup lang="ts">
+import { string } from "vue-types";
+const props = defineProps({
+    title: string(),
+})
+</script>
 
 <template>
+
+    <Head :title="title" />
     <div class="relative">
         <div class="bg-primary fixed top-0 p-8 left-0 right-0 z-50">
             <nav class="flex items-center justify-between flex-wrap">
@@ -8,15 +21,96 @@
                     <img src="logo/dasi_logo_type.svg" alt="" srcset="">
                 </div>
                 <div class="block lg:hidden">
-                    <button
-                        class="flex items-center px-3 py-2 border rounded text-white  hover:text-primary hover:border-white">
-                        <svg class="h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                            <title>Tentang</title>
-                            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-                        </svg>
-                    </button>
+
+                    <Popover v-slot="{ open }" class="relative">
+                        <PopoverButton :class="open ? '' : 'text-opacity-90'"
+                            class="flex items-center px-3 py-2 border rounded text-white  hover:text-primary hover:border-white">
+
+                            <svg class="h-3 w-3 fill-white" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                <title>Tentang</title>
+                                <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+                            </svg>
+                        </PopoverButton>
+
+                        <transition enter-active-class="transition duration-200 ease-out"
+                            enter-from-class="translate-y-1 opacity-0" enter-to-class="translate-y-0 opacity-100"
+                            leave-active-class="transition duration-150 ease-in"
+                            leave-from-class="translate-y-0 opacity-100" leave-to-class="translate-y-1 opacity-0">
+                            <PopoverPanel
+                                class="absolute left-[120%] z-10 mt-3 w-screen max-w-sm -translate-x-full transform px-0">
+                                <div class="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
+                                    <div class="relative grid gap-8 bg-white p-7 lg:grid-cols-2">
+                                        <a href="#about"
+                                            class="-m-3 flex items-center rounded-lg p-2 transition duration-150 ease-in-out hover:bg-gray-50 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50">
+                                            <div
+                                                class="flex h-10 w-10 shrink-0 items-center justify-center text-white sm:h-12 sm:w-12">
+                                                <div class="bg-violet-100 p-4 rounded-lg">
+                                                    <InformationCircleIcon class="h-6 w-6 text-primary" />
+                                                </div>
+                                            </div>
+                                            <div class="ml-4">
+                                                <p class="text-sm font-medium text-gray-900">
+                                                    Tentang
+                                                </p>
+                                                <p class="text-sm text-gray-500">
+                                                    Ketahui apa itu DASI
+                                                </p>
+                                            </div>
+                                        </a>
+                                        <a href="#feature"
+                                            class="-m-3 flex items-center rounded-lg p-2 transition duration-150 ease-in-out hover:bg-gray-50 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50">
+                                            <div
+                                                class="flex h-10 w-10 shrink-0 items-center justify-center text-white sm:h-12 sm:w-12">
+                                                <div class="bg-violet-100 p-4 rounded-lg">
+                                                    <ViewColumnsIcon class="h-6 w-6 text-primary" />
+                                                </div>
+                                            </div>
+                                            <div class="ml-4">
+                                                <p class="text-sm font-medium text-gray-900">
+                                                    Fitur
+                                                </p>
+                                                <p class="text-sm text-gray-500">
+                                                    Fitur apa saja yang ditawarkan DASI
+                                                </p>
+                                            </div>
+                                        </a>
+                                        <a href="#video"
+                                            class="-m-3 flex items-center rounded-lg p-2 transition duration-150 ease-in-out hover:bg-gray-50 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50">
+                                            <div
+                                                class="flex h-10 w-10 shrink-0 items-center justify-center text-white sm:h-12 sm:w-12">
+                                                <div class="bg-violet-100 p-4 rounded-lg">
+                                                    <PlayIcon class="h-6 w-6 text-primary" />
+                                                </div>
+                                            </div>
+                                            <div class="ml-4">
+                                                <p class="text-sm font-medium text-gray-900">
+                                                    Video
+                                                </p>
+                                                <p class="text-sm text-gray-500">
+                                                    Video singkat tentang DASI
+                                                </p>
+                                            </div>
+                                        </a>
+                                    </div>
+                                    <div class="bg-gray-50 p-4">
+                                        <a href="##"
+                                            class="flow-root rounded-md px-2 py-2 transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50">
+                                            <span class="flex items-center">
+                                                <span class="text-sm font-medium text-gray-900">
+                                                    Dokumentasi
+                                                </span>
+                                            </span>
+                                            <span class="block text-sm text-gray-500">
+                                                Mulai mengintegrasikan data dengan membaca dokumentasi.
+                                            </span>
+                                        </a>
+                                    </div>
+                                </div>
+                            </PopoverPanel>
+                        </transition>
+                    </Popover>
                 </div>
-                <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+                <div class="w-full hidden flex-grow lg:flex lg:items-center lg:w-auto">
                     <div class="text-xl font-bold lg:flex-grow">
                         <a href="#about"
                             class="block mt-4 active:underline lg:inline-block lg:mt-0 text-white hover:text-purple-200 mr-16">
@@ -32,49 +126,51 @@
                         </a>
                     </div>
                     <div>
-                        <a href="#"
-                            class="inline-block text-3xl font-bold px-4 py-2 leading-none border rounded-3xl text-primary border-white bg-white hover:border-transparent hover:text-primary hover:bg-purple-300 mt-4 lg:mt-0">LOGIN</a>
+                        <Link :href="route('login')"
+                            class="inline-block text-3xl font-bold px-4 py-2 leading-none border rounded-3xl text-primary border-white bg-white hover:border-transparent hover:text-primary hover:bg-purple-300 mt-4 lg:mt-0">
+                        LOGIN</Link>
                     </div>
                 </div>
             </nav>
         </div>
     </div>
 
-    <section class="relative w-full bg-primary pb-40 h-screen overflow-clip align-content-center">
-        <div class="container m-auto">
-            <div class="flex flex-wrap md:px-0 justify-between">
-                <div class="justify-center flex-col lg:w-2/3 flex h-screen">
-                    <div class="font-bold text-xl text-white md:text-6xl w-full">
+    <section class="relative w-full bg-primary lg:pb-40 lg:h-screen overflow-clip align-content-center">
+        <div class="container m-auto p-8 lg:p-0">
+            <div class="flex flex-col lg:flex-wrap md:px-0 justify-between">
+                <div class="justify-center flex-col lg:w-2/3 flex h-screen w-full">
+                    <div class="font-bold text-3xl text-white md:text-6xl w-full">
                         <p class="mb-4"><span class="text-yellow-300">Integrasikan</span> Angka Daerah </p>
                         <p> Kita Lewat <span class="text-yellow-300">DASI</span></p>
                     </div>
 
-                    <p class="mt-8 text-2xl text-gray-300 lg:w-10/12">DASI (Dalam Angka System Informasi) merupakan
+                    <p class="mt-8 lg:text-2xl text-gray-300 lg:w-10/12">DASI (Dalam Angka System Informasi) merupakan
                         sistem
                         informasi
                         berbasis web yang bertujuan untuk diseminasi publikasi
                         Daerah Dalam Angka
                     </p>
 
-                    <button
+                    <Link :href="route('login')"
                         class="mt-16 bg-yellow-300 hover:bg-yellow-100 rounded-3xl px-4 py-2 text-3xl font-bold w-fit">
-                        MULAI
-                    </button>
+                    MULAI
+                    </Link>
 
                 </div>
-                <div class="lg:w-5/12 -mb-[calc(6rem)] right-0 bottom-0 vh-100 animate-pulse duration-300 absolute">
-                    <img src="images/landing_illustration.png" class="relative" alt="food illustration">
+                <div
+                    class="lg:w-5/12 w-full -mb-8 ml-12 sm:ml-48 lg:-mb-[calc(6rem)] lg:right-0 lg:bottom-0  animate-pulse duration-300 lg:absolute">
+                    <img src="images/landing_illustration.png" class="relative">
                 </div>
             </div>
         </div>
     </section>
     <section id="about" class="bg-white p-8 flex justify-between flex-col w-full">
-        <div class="flex flex-wrap min-h-screen">
-            <div class="w-2/3 inline-block align-middle m-auto p-8">
+        <div class="flex flex-col lg:flex-row min-h-screen lg:justify-evenly">
+            <div class="lg:w-2/3 w-full inline-block align-middle m-auto p-8">
                 <img v-lazy="{ src: 'images/buku.png', loading: 'images/loading-blog.gif', error: 'images/200x200.png' }"
                     alt="" srcset="">
             </div>
-            <div class="w-1/3 m-auto text-left text-xl font-semibold">
+            <div class="lg:w-1/3 w-full m-auto text-left lg:text-xl font-semibold">
                 <p>
                     Beragam jenis data tersedia dalam publikasi
                     Daerah Dalam Angka (DDA) yang dimuat mulai dari
@@ -102,8 +198,8 @@
             </div>
         </div>
         <div class="flex flex-col w-full my-auto min-h-screen mx-auto justify-center">
-            <div class="w-1/2 mx-auto text-center">
-                <p class="text-xl font-semibold">
+            <div class="lg:w-1/2 mx-auto text-center">
+                <p class="lg:text-xl font-semibold">
                     Oleh karenanya, Dalam Angka System Information hadir
                     untuk dapat menghadirkan Integrasi secara digital
                     semua data yang dibutuhkan dalam penyusunan
