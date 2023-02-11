3637be00007c8f251e73b8bba556e844ed28b8e0
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2023-02-11 19:52:57 +0800

    ✨ feat(package): install package
    install tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const plugin = require("tailwindcss/plugin");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
@@ -6,7 +8,22 @@ module.exports = {
         "./resources/**/*.vue",
     ],
     theme: {
-        extend: {},
+        extend: {
+            boxShadow: {
+                DEFAULT: "0px 8px 20px rgba(143, 142, 174, 0.15)",
+                md: "0 4px 6px -1px rgba(0, 0, 0, 0.08), 0 2px 4px -1px rgba(0, 0, 0, 0.02)",
+                lg: "0 10px 15px -3px rgba(0, 0, 0, 0.08), 0 4px 6px -2px rgba(0, 0, 0, 0.01)",
+                xl: "0 20px 25px -5px rgba(0, 0, 0, 0.08), 0 10px 10px -5px rgba(0, 0, 0, 0.01)",
+            },
+        },
     },
-    plugins: [],
+    plugins: [require("@tailwindcss/forms")],
+    css: {
+        postCss: {
+            plugins: {
+                tailwindcss: {},
+                autoprefixer: {},
+            },
+        },
+    },
 };
