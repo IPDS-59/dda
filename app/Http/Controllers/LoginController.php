66146cd8157285c6adc8dc42ee1b84d8269d3ec7
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2023-02-12 05:10:37 +0800

    🦄 refactor(controller): edit controller
    modify login controller to only return inertia response to render login page and use fortify backend instead

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,48 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class LoginController extends Controller
 {
     //
     public function index()
     {
-        return view('login', [
-            'title' => 'login',
-            'title2' => 'test',
-        ]);
-    }
-
-    public function authenticate(Request $request)
-    {
-        $credentials = $request->validate([
-            'username' => 'required',
-            'password' => 'required',
-        ]);
-
-        if (Auth::attempt($credentials)) {
-            $request->session()->regenerate();
-            if (auth()->user()->role == 1) {
-                return redirect()->route('admin.dashboard');
-            } elseif (auth()->user()->role == 2) {
-                return redirect()->route('user.dashboard');
-            } else {
-                return redirect('/login');
-            }
-        }
-
-        return back()->with('loginError', 'Username atau Password salah!');
-        // dd('berhasil login');
-    }
-
-    public function logout(Request $request)
-    {
-        Cookie::queue(Cookie::make('last_username', auth()->user()->username, 2628000));
-        Auth::logout();
-        $request->session()->invalidate();
-        $request->session()->regenerateToken();
-
-        return redirect('/login');
+        return Inertia::render(
+            'auth/login/index',
+            [
+                'title' => 'Masuk',
+            ]
+        );
     }
 }
