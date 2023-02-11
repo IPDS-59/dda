ddc8c9d6969ecd05e184ca9e03c95a407063fd06
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2023-02-12 05:55:11 +0800

    🧪 test(pages): add test for page
    add test for landing page and login page

--- tests/Feature/LandingPageTest.php
+++ tests/Feature/LandingPageTest.php
@@ -3,9 +3,10 @@
 use Inertia\Testing\AssertableInertia as Assert;
 
 it('has landing page return landing.vue page', function () {
-    $response = $this->get('/');
+    $response = $this->get(route('landing'));
 
     $response->assertInertia(function (Assert $page) {
         $page->component('landing');
+        $page->hasAll(['title']);
     });
 });
