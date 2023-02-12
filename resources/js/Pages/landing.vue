ce6ae1c4ec20947dbc15085f755862274c676d3c
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2023-02-12 22:35:21 +0800

    🦄 refactor(pages): implement translate
    implement translation in landing page

--- resources/js/Pages/landing.vue
+++ resources/js/Pages/landing.vue
@@ -2,7 +2,9 @@
 <script lang="ts">
 import { Head, Link } from "@inertiajs/vue3";
 import { Popover, PopoverButton, PopoverPanel } from '@headlessui/vue';
-import { InformationCircleIcon, ViewColumnsIcon, NewspaperIcon, PlayIcon } from '@heroicons/vue/24/solid';
+import { InformationCircleIcon, ViewColumnsIcon, ChevronDownIcon, PlayIcon } from '@heroicons/vue/24/solid';
+import { trans, loadLanguageAsync } from 'laravel-vue-i18n';
+import route from 'ziggy-js'
 </script>
 <script setup lang="ts">
 import { string } from "vue-types";
@@ -50,10 +52,10 @@ const props = defineProps({
                                             </div>
                                             <div class="ml-4">
                                                 <p class="text-sm font-medium text-gray-900">
-                                                    Tentang
+                                                    {{ trans('landing.navigation.about.title') }}
                                                 </p>
                                                 <p class="text-sm text-gray-500">
-                                                    Ketahui apa itu DASI
+                                                    {{ trans('landing.navigation.about.description') }}
                                                 </p>
                                             </div>
                                         </a>
@@ -67,10 +69,10 @@ const props = defineProps({
                                             </div>
                                             <div class="ml-4">
                                                 <p class="text-sm font-medium text-gray-900">
-                                                    Fitur
+                                                    {{ trans('landing.navigation.feature.title') }}
                                                 </p>
                                                 <p class="text-sm text-gray-500">
-                                                    Fitur apa saja yang ditawarkan DASI
+                                                    {{ trans('landing.navigation.feature.description') }}
                                                 </p>
                                             </div>
                                         </a>
@@ -84,10 +86,10 @@ const props = defineProps({
                                             </div>
                                             <div class="ml-4">
                                                 <p class="text-sm font-medium text-gray-900">
-                                                    Video
+                                                    {{ trans('landing.navigation.video.title') }}
                                                 </p>
                                                 <p class="text-sm text-gray-500">
-                                                    Video singkat tentang DASI
+                                                    {{ trans('landing.navigation.video.description') }}
                                                 </p>
                                             </div>
                                         </a>
@@ -97,11 +99,11 @@ const props = defineProps({
                                             class="flow-root rounded-md px-2 py-2 transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50">
                                             <span class="flex items-center">
                                                 <span class="text-sm font-medium text-gray-900">
-                                                    Dokumentasi
+                                                    {{ trans('landing.navigation.documentation.title') }}
                                                 </span>
                                             </span>
                                             <span class="block text-sm text-gray-500">
-                                                Mulai mengintegrasikan data dengan membaca dokumentasi.
+                                                {{ trans('landing.navigation.documentation.description') }}
                                             </span>
                                         </a>
                                     </div>
@@ -114,21 +116,71 @@ const props = defineProps({
                     <div class="text-xl font-bold lg:flex-grow">
                         <a href="#about"
                             class="block mt-4 active:underline lg:inline-block lg:mt-0 text-white hover:text-purple-200 mr-16">
-                            Tentang
+                            {{ trans('landing.navigation.about.title') }}
                         </a>
                         <a href="#feature"
                             class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-purple-200 mr-16">
-                            Fitur
+                            {{ trans('landing.navigation.feature.title') }}
                         </a>
                         <a href="#responsive-header"
                             class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-purple-200">
-                            Video
+                            {{ trans('landing.navigation.video.title') }}
                         </a>
                     </div>
-                    <div>
+                    <div class="flex flex-row items-center">
+                        <Popover v-slot="{ open }" class="relative mr-6">
+                            <PopoverButton :class="open ? '' : 'text-opacity-90'"
+                                class="flex items-center px-3 py-2 border rounded text-white  hover:text-gray-400 hover:border-gray-400">
+                                {{ trans('landing.button.language') }}
+                                <ChevronDownIcon class="h-6 w-6 text-white ml-4" />
+                            </PopoverButton>
+
+                            <transition enter-active-class="transition duration-200 ease-out"
+                                enter-from-class="translate-y-1 opacity-0" enter-to-class="translate-y-0 opacity-100"
+                                leave-active-class="transition duration-150 ease-in"
+                                leave-from-class="translate-y-0 opacity-100" leave-to-class="translate-y-1 opacity-0">
+                                <PopoverPanel
+                                    class="absolute left-[120%] z-10 mt-3 w-screen max-w-sm -translate-x-full transform px-0">
+                                    <div class="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
+                                        <div class="relative grid gap-8 bg-white p-7 ">
+                                            <a @click.prevent="loadLanguageAsync('id')"
+                                                class="-m-3 flex items-center rounded-lg p-2 transition duration-150 ease-in-out hover:bg-gray-50 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50">
+                                                <div
+                                                    class="flex  shrink-0 items-center justify-center text-white  text-4xl">
+                                                    <flag iso="id" />
+                                                </div>
+                                                <div class="ml-4">
+                                                    <p class="text-sm font-medium text-gray-900">
+                                                        {{ trans('landing.language.indonesia.title') }}
+                                                    </p>
+                                                    <p class="text-sm text-gray-500">
+                                                        {{ trans('landing.language.indonesia.description') }}
+                                                    </p>
+                                                </div>
+                                            </a>
+                                            <a @click.prevent="loadLanguageAsync('en')"
+                                                class="-m-3 flex items-center rounded-lg p-2 transition duration-150 ease-in-out hover:bg-gray-50 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50">
+                                                <div
+                                                    class="flex  shrink-0 items-center justify-center text-white  text-4xl">
+                                                    <flag iso="gb" />
+                                                </div>
+                                                <div class="ml-4">
+                                                    <p class="text-sm font-medium text-gray-900">
+                                                        {{ trans('landing.language.english.title') }}
+                                                    </p>
+                                                    <p class="text-sm text-gray-500">
+                                                        {{ trans('landing.language.english.description') }}
+                                                    </p>
+                                                </div>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </PopoverPanel>
+                            </transition>
+                        </Popover>
                         <Link :href="route('login')"
                             class="inline-block text-3xl font-bold px-4 py-2 leading-none border rounded-3xl text-primary border-white bg-white hover:border-transparent hover:text-primary hover:bg-purple-300 mt-4 lg:mt-0">
-                        LOGIN</Link>
+                        {{ trans('landing.button.login').toUpperCase() }}</Link>
                     </div>
                 </div>
             </nav>
@@ -140,20 +192,21 @@ const props = defineProps({
             <div class="flex flex-col lg:flex-wrap md:px-0 justify-between">
                 <div class="justify-center flex-col lg:w-2/3 flex h-screen w-full">
                     <div class="font-bold text-3xl text-white md:text-6xl w-full">
-                        <p class="mb-4"><span class="text-yellow-300">Integrasikan</span> Angka Daerah </p>
-                        <p> Kita Lewat <span class="text-yellow-300">DASI</span></p>
+                        <p class="mb-4"><span class="text-yellow-300">{{ trans('landing.hero.integrate') }}</span> {{
+                            trans('landing.hero.regional_statistics')
+                        }} </p>
+                        <p> {{ trans('landing.hero.our_through') }} <span class="text-yellow-300">{{
+                            trans('landing.hero.dasi')
+                        }}</span></p>
                     </div>
 
-                    <p class="mt-8 lg:text-2xl text-gray-300 lg:w-10/12">DASI (Dalam Angka System Informasi) merupakan
-                        sistem
-                        informasi
-                        berbasis web yang bertujuan untuk diseminasi publikasi
-                        Daerah Dalam Angka
+                    <p class="mt-8 lg:text-2xl text-gray-300 lg:w-10/12">
+                        {{ trans('landing.hero.dasi_desc') }}
                     </p>
 
                     <Link :href="route('login')"
                         class="mt-16 bg-yellow-300 hover:bg-yellow-100 rounded-3xl px-4 py-2 text-3xl font-bold w-fit">
-                    MULAI
+                    {{ trans('landing.button.getting_started') }}
                     </Link>
 
                 </div>
@@ -166,48 +219,17 @@ const props = defineProps({
     </section>
     <section id="about" class="bg-white p-8 flex justify-between flex-col w-full">
         <div class="flex flex-col lg:flex-row min-h-screen lg:justify-evenly">
-            <div class="lg:w-2/3 w-full inline-block align-middle m-auto p-8">
+            <div class="lg:w-1/3 w-full inline-block align-middle m-auto p-8">
                 <img v-lazy="{ src: 'images/buku.png', loading: 'images/loading-blog.gif', error: 'images/200x200.png' }"
                     alt="" srcset="">
             </div>
-            <div class="lg:w-1/3 w-full m-auto text-left lg:text-xl font-semibold">
-                <p>
-                    Beragam jenis data tersedia dalam publikasi
-                    Daerah Dalam Angka (DDA) yang dimuat mulai dari
-                    level Nasional hingga level Kabupaten/Kota.
-                    <br>
-                    <br>
-                    Publikasi Daerah Dalam Angka ini memuat
-                    lengkap statistik yang ada pada daerah tersebut
-                    mulai dari kondisi Sosial dan Kependudukan,
-                    Ekonomi dan Perdagangan, serta Pertanian dan
-                    Pertambangan di Indonesia.
-                    <br>
-                    <br>
-                    Sajian lengkap ini tentunya amat berguna baik untuk
-                    perencanaan maupun evaluasi pembangunan
-                    suatu daerah kedepannya.
-                    <br>
-                    <br>
-                    Namun, hadirnya publikasi ini tidak lepas dari semua
-                    pihak. Walaupun penyusun dari publikasi ini
-                    merupakan Badan Pusat Statistik. Namun,
-                    keterlibatan Kementerian, Lembaga, Dinas, dan
-                    Instansi amat diperlukan guna proses terbitnya DDA.
-                </p>
+            <div class="lg:w-1/2 w-full m-auto text-left lg:text-xl font-semibold">
+                <p v-html="trans('landing.about')" />
             </div>
         </div>
         <div class="flex flex-col w-full my-auto min-h-screen mx-auto justify-center">
             <div class="lg:w-1/2 mx-auto text-center">
-                <p class="lg:text-xl font-semibold">
-                    Oleh karenanya, Dalam Angka System Information hadir
-                    untuk dapat menghadirkan Integrasi secara digital
-                    semua data yang dibutuhkan dalam penyusunan
-                    Publikasi Derah Dalam Angka.
-                    <br>
-                    <br>
-                    Mari Integrasikan Angka Daerah kita lewat DASI
-                </p>
+                <p class="lg:text-xl font-semibold" v-html="trans('landing.about_second')" />
             </div>
             <div class="mx-auto mt-16">
                 <img v-lazy="{ src: 'logo/dasi_logo_primary.svg', loading: 'images/loading-blog.gif', error: 'images/200x200.png' }"
