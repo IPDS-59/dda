33ef38d0a049125f108cef14dc744b6ccd2e7e2c
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2023-02-12 22:34:30 +0800

    ✨ feat(lang): add lang trans
    add language translations for text in landing page

---
+++ lang/en/landing.php
@@ -0,0 +1,74 @@
+<?php
+
+
+return [
+    'navigation' => [
+        'about' => [
+            'title' => 'About',
+            'description' => 'Getting to know what DASI is'
+        ],
+        'feature' => [
+            'title' => 'Feature',
+            'description' => 'What features does DASI offers'
+        ],
+        'video' => [
+            'title' => 'Video',
+            'description' => 'Short video about DASI'
+        ],
+        'documentation' => [
+            'title' => 'Documentation',
+            'description' => 'Start integrating data by reading our documentation'
+        ]
+    ],
+    'button' => [
+        'login' => 'Login',
+        'getting_started' => 'Getting Started',
+        'language' => 'Language',
+    ],
+    'language' => [
+        'indonesia' => [
+            'title' => 'Bahasa Indonesia',
+            'description' => 'Change to Bahasa'
+        ],
+        'english' => [
+            'title' => 'English',
+            'description' => 'Change to English'
+        ]
+    ],
+    'hero' => [
+        'integrate' => 'Integrate',
+        'regional_statistics' => 'Our Regional Data',
+        'our_through' => 'Through',
+        'dasi' => 'DASI',
+        'dasi_desc' => 'DASI (In Figures Information System) is a web-based information system that aims to disseminate the publication of Regions in Figures'
+    ],
+    'about' => 'Various types of data are available in publications
+                     Regions in Figures (DDA) which is loaded starting from
+                     National level to Regency/City level.
+                    <br>
+                    <br>
+                    This Regional Publication in Figures contains
+                    complete statistics in the area
+                    starting from Social and Population conditions,
+                    Economy and Trade, and Agriculture and
+                    Mining in Indonesia.
+                    <br>
+                    <br>
+                    This complete dish is of course very useful for both
+                    development planning and evaluation
+                    an area in the future.
+                    <br>
+                    <br>
+                    However, the presence of this publication can not be separated from all
+                    party. Although the authors of this publication
+                    is the Central Bureau of Statistics. However,
+                    involvement of Ministries, Agencies, Services, and
+                    Institutions are very necessary for the process of issuance of the DDA.',
+    'about_second' => 'Therefore, Dalam Angka System Informasi is present
+                     to be able to deliver digital integration
+                     all the data needed in the preparation
+                     Regional Publications in Figures.
+                    <br>
+                    <br>
+                    Let\'s integrate our regional figures through DASI'
+];
